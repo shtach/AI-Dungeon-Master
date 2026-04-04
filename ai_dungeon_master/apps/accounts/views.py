f826a5d186ab8d03968deeb309e4eed2f4405b8c
shtach <tar.sherbuna@gmail.com>
2026-04-04 16:58:54 +0200

    feat(accounts): add login, register, logout views and URLs

--- ai_dungeon_master/apps/accounts/views.py
+++ ai_dungeon_master/apps/accounts/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class CustomLoginView(LoginView):
+    template_name = "accounts/login.html"
+
+
+class CustomLogoutView(LogoutView):
+    pass
+
+
+class RegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = "accounts/register.html"
+    success_url = reverse_lazy("accounts:login")
