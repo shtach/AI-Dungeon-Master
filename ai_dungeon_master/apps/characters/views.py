093681f7fa74bf283da8a7ce447b16ea2bdbb445
Ivan <s181426@student.uken.krakow.pl>
2026-04-14 09:24:23 +0200

    feat(characters): add CharacterCreationForm, CreateView and URLs for character generation

--- ai_dungeon_master/apps/characters/views.py
+++ ai_dungeon_master/apps/characters/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# Create your views here.
+from .models import Character
+from .forms import CharacterCreationForm
+
+class CharacterCreateView(LoginRequiredMixin, CreateView):
+    model = Character
+    form_class = CharacterCreationForm
+    template_name = "characters/create.html"
+    success_url = reverse_lazy("game:dashboard")
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
