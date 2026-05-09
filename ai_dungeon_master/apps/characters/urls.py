be8eb7974459bcfa4deb03dc0ba5a25c904f8849
Ivan <s181426@student.uken.krakow.pl>
2026-05-09 15:29:37 +0200

    feat(characters): add wizard step 2 logic and validation for character class

--- ai_dungeon_master/apps/characters/urls.py
+++ ai_dungeon_master/apps/characters/urls.py
@@ -6,7 +6,7 @@ app_name = "characters"
 
 urlpatterns = [
     path("create/step1/", views.CharacterCreateStep1View.as_view(), name="create_step1"),
-    path("create/step2/", TemplateView.as_view(template_name="characters/create_step2.html"), name="create_step2"),
+    path("create/step2/", views.CharacterCreateStep2View.as_view(), name="create_step2"),
     path("create/step3/", TemplateView.as_view(template_name="characters/create_step3.html"), name="create_step3"),
     path("create/step4/", TemplateView.as_view(template_name="characters/create_step4.html"), name="create_step4"),
 ]
--- ai_dungeon_master/apps/characters/views.py
+++ ai_dungeon_master/apps/characters/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 WIZARD_SESSION_KEY = 'character_wizard'
 
+
 class CharacterCreateStep1View(LoginRequiredMixin, View):
     def get(self, request):
         wizard_data = request.session.get(WIZARD_SESSION_KEY, {})
@@ -21,3 +22,29 @@ class CharacterCreateStep1View(LoginRequiredMixin, View):
         request.session[WIZARD_SESSION_KEY] = wizard_data
 
         return redirect("characters:create_step2")
+
+
+class CharacterCreateStep2View(LoginRequiredMixin, View):
+    def get(self, request):
+        wizard_data = request.session.get(WIZARD_SESSION_KEY, {})
+
+        if not wizard_data.get("name"):
+            return redirect("characters:create_step1")
+
+        return render(request, "characters/create_step2.html", {
+            "wizard_data": wizard_data,
+            "available_classes": ["Warrior", "Wizard", "Rogue", "Cleric"]
+        })
+
+    def post(self, request):
+        character_class = request.POST.get("character_class", "").strip()
+
+        allowed_classes = ["Warrior", "Wizard", "Rogue", "Cleric"]
+        if character_class not in allowed_classes:
+            return redirect("characters:create_step2")
+
+        wizard_data = request.session.get(WIZARD_SESSION_KEY, {})
+        wizard_data["character_class"] = character_class
+        request.session[WIZARD_SESSION_KEY] = wizard_data
+
+        return redirect("characters:create_step3")
