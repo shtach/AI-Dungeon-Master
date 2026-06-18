227ee519abef48249e269c39aecc2e7d6d172b94
Ivan <s181426@student.uken.krakow.pl>
2026-06-18 23:25:14 +0200

    feat: register combat app in INSTALLED_APPS

--- ai_dungeon_master/config/settings/base.py
+++ ai_dungeon_master/config/settings/base.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "ai_dungeon_master.apps.accounts",
     "ai_dungeon_master.apps.characters",
     "ai_dungeon_master.apps.game",
+    "ai_dungeon_master.apps.game.combat",
     "ai_dungeon_master.apps.world",
     "ai_dungeon_master.apps.core",
 ]
