ea400e4c45b50c5d1ad23b424841359c06116a08
Ivan <s181426@student.uken.krakow.pl>
2026-06-18 23:24:51 +0200

    feat: add CombatConfig for combat app

---
+++ ai_dungeon_master/apps/game/combat/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class CombatConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "ai_dungeon_master.apps.game.combat"
+    verbose_name = "Combat"
