1486290e76f5b361f209a75a65b2e13ca41a5ea1
Ivan <s181426@student.uken.krakow.pl>
2026-06-19 01:49:39 +0200

    feat: add QuestsConfig for quests app

---
+++ ai_dungeon_master/apps/game/quests/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class QuestsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "ai_dungeon_master.apps.game.quests"
+    verbose_name = "Quests"
