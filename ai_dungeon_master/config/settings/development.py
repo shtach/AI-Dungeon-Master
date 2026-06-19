579b21be7fa7b03e53d38f4deda5c1b032843873
shtach <tar.sherbuna@gmail.com>
2026-06-19 12:00:39 +0200

    feat(game): inventory screen with equip slots + compare

--- ai_dungeon_master/config/settings/development.py
+++ ai_dungeon_master/config/settings/development.py
@@ -19,11 +19,26 @@ INSTALLED_APPS += [
     "debug_toolbar",
 ]
 
-MIDDLEWARE += [
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
-]
+MIDDLEWARE.insert(0, "debug_toolbar.middleware.DebugToolbarMiddleware")
 
 INTERNAL_IPS = ["127.0.0.1"]
+
+DEBUG_TOOLBAR_PANELS = [
+    "debug_toolbar.panels.history.HistoryPanel",
+    "debug_toolbar.panels.versions.VersionsPanel",
+    "debug_toolbar.panels.timer.TimerPanel",
+    "debug_toolbar.panels.settings.SettingsPanel",
+    "debug_toolbar.panels.headers.HeadersPanel",
+    "debug_toolbar.panels.request.RequestPanel",
+    "debug_toolbar.panels.sql.SQLPanel",
+    "debug_toolbar.panels.staticfiles.StaticFilesPanel",
+    # TemplatesPanel disabled: patches Template._render and recurses with django-tailwind
+    "debug_toolbar.panels.alerts.AlertsPanel",
+    "debug_toolbar.panels.cache.CachePanel",
+    "debug_toolbar.panels.signals.SignalsPanel",
+    "debug_toolbar.panels.redirects.RedirectsPanel",
+    "debug_toolbar.panels.profiling.ProfilingPanel",
+]
 
 # Show logs
 LOGGING["handlers"]["console_verbose"] = {
